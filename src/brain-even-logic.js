3144b8e65a2ed42f8041986e750cf001d29679dd
sergey.lebedev <s.lebedev@cometrica.ru>
2020-11-02 22:52:56 +0300

    Feature: Step 5. Part 3. Have finished Step 5.

--- src/brain-even-logic.js
+++ src/brain-even-logic.js
@@ -1,6 +1,10 @@
 import promptly from 'promptly';
 
-import userGreeting from './cli.js';
+let guessNumber = 0;
+
+const userGreeting = async () => (
+  promptly.prompt('May I have your name?:')
+);
 
 const numberGenRandom = () => Math.floor((Math.random() * 100) + 1);
 
@@ -8,40 +12,37 @@ const askUserNumber = (questionNumber) => {
   console.log('Question', questionNumber);
 };
 
-export const playConditions = async (userName) => {
-  const counter = 0;
-  const continueAnswerPhrase = 'Correct!';
-  const winnerPhrase = `Congratulations, ${userName}!}`;
-  const questionNumber = numberGenRandom();
+const decrement = () => (() => {
+  guessNumber += 1;
+  return guessNumber;
+});
 
-  const decrement = (num) => {
-    let temp = num;
-    temp += 1;
-    return temp;
-  };
+const playConditions = async (userName) => {
+  const counter = decrement();
+  const continueAnswerPhrase = 'Correct!';
+  const winnerPhrase = `Congratulations, ${userName}!`;
+  const questionNumber = numberGenRandom();
 
   askUserNumber(questionNumber);
 
   await promptly.prompt('Your answer:')
     .then((response) => {
-      const correctAnswerPhrase = response === 'yes' ? 'no' : 'yes';
-      const incorrectAnswerPhrase = `${response} is wrong answer ;(. Correct answer was ${correctAnswerPhrase}. Let's try again, ${userName}!`;
+      const userAnswerPhrase = response === 'yes' ? 'no' : 'yes';
+      const incorrectAnswerPhrase = (responseAnswer, correctPhrase, name) => (
+        console.log(`'${responseAnswer}' is wrong answer ;(. Correct answer was '${correctPhrase}'.\nLet's try again, ${name}!`)
+      );
+
+      counter();
+
       if ((questionNumber % 2 === 0 && response === 'yes') || (questionNumber % 2 !== 0 && response === 'no')) {
         console.log(continueAnswerPhrase);
-        decrement(counter);
-        askUserNumber(questionNumber);
-        playConditions(userName);
-        if (counter === 3) {
-          console.log(winnerPhrase);
-        }
-      } else {
-        console.log(incorrectAnswerPhrase);
-        console.log('make lint');
+        return guessNumber === 3 ? console.log(winnerPhrase) : playConditions(userName);
       }
+      return incorrectAnswerPhrase(response, userAnswerPhrase, userName);
     });
 };
 
-export const brainEvenLogic = () => {
+const brainEvenLogic = () => {
   console.log('Welcome to the Brain Games!');
   let userName;
   userGreeting()
@@ -52,3 +53,5 @@ export const brainEvenLogic = () => {
     .then(() => console.log('Answer "yes" if the number is even, otherwise answer "no".'))
     .then(() => playConditions(userName));
 };
+
+export default brainEvenLogic;
